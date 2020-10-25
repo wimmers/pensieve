eef48fefc52459e76334c57c252b4cd558dc2584
Simon Wimmer <wimmers@in.tum.de>
2020-10-25 13:43:53 +0100

    feat(js) allow editing of info JSON

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import GraphView from './GraphView';
 import MarkdownEditor from '@uiw/react-markdown-editor';
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/theme/monokai.css';
 
 class App extends Component {
   state = {
@@ -17,8 +21,27 @@ class App extends Component {
   }
 
   updateMarkdown = (_editor, _data, value) => {
+    if (!this.selected)
+      return
     const cy = this.graphView.current.cy
-    cy.$(`[id = '${this.state.selected.id}']`).json({ "data": { "note": value } })
+    cy.$(`[id = '${this.state.selected.id}']`).json({ data: { note: value } })
+  }
+
+  updateJson = (editor) => {
+    if (!this.selected)
+      return
+    try {
+      const code = editor.getValue()
+      const info = JSON.parse(code)
+      const cy = this.graphView.current.cy
+      const data = { data: { info: info, label: info['name'] } }
+      console.log(data)
+      cy.$(`[id = '${this.state.selected.id}']`).json(data)
+    }
+    catch (e) {
+      if (e.name !== "SyntaxError")
+        throw e
+    }
   }
 
   render() {
@@ -30,12 +53,27 @@ class App extends Component {
         </div>
       </Container>,
       <Container fluid className="p-3">
-        <MarkdownEditor
-          value={state.selected ? state.selected.note : null}
-          onChange={this.updateMarkdown}
-          visible={false}
-          height={500}
-        />
+        <Row>
+          <Col xs="12" lg="8" xl="9">
+            <MarkdownEditor
+              value={state.selected ? state.selected.note : null}
+              onChange={this.updateMarkdown}
+              visible={false}
+              height={500}
+            />
+          </Col>
+          <Col xs="12" lg="4" xl="3">
+            <CodeMirror
+              value={state.selected ? JSON.stringify(state.selected.info, null, 4) : null}
+              onChange={this.updateJson}
+              options={{
+                theme: 'monokai',
+                tabSize: 2,
+                mode: 'json',
+              }}
+            />
+          </Col>
+        </Row>
       </Container>
     ]);
   }
