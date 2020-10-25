f9a1f96cba9ff79049cdf44208d68846ab2d813f
Simon Wimmer <wimmers@in.tum.de>
2020-10-25 18:25:57 +0100

    feat(js) basic actions: add nodes & links, refresh

--- src/App.js
+++ src/App.js
@@ -4,17 +4,91 @@ import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import GraphView from './GraphView';
 import MarkdownEditor from '@uiw/react-markdown-editor';
 import CodeMirror from '@uiw/react-codemirror';
 import 'codemirror/theme/monokai.css';
 
+const empty_info = {
+  "files": null,
+  "deleted": null,
+  "favorited": null,
+  "pinned": null,
+  "tags": null,
+  "name": null,
+  "created": null,
+  "modified": null
+}
+
 class App extends Component {
   state = {
-    selected: undefined
+    selected: undefined,
+    adding: null,
+    linking: null
   }
 
   graphView = React.createRef();
+
+  markdownEditor = React.createRef()
+
+  onAddNote = data => {
+    this.setState({ ...this.state, adding: "note" })
+  }
+
+  onAddFrom = data => {
+    this.setState({ ...this.state, linking: "from" })
+  }
+
+  onAddCrossref = data => {
+    this.setState({ ...this.state, linking: "crossref" })
+  }
+
+  unsetFlags = () => {
+    this.setState({...this.state, adding: null, linking: null})
+  }
+
+  insertTextAtCursor(text) {
+    const editor = this.markdownEditor.current.CodeMirror.editor
+    console.log(editor)
+    var doc = editor.getDoc();
+    var cursor = doc.getCursor();
+    doc.replaceRange(text, cursor);
+  }
+
+  onTapNode = node => {
+    const state = this.state
+    if (state.linking && state.selected) {
+      console.log(state.selected)
+      const style = state.linking === 'crossref' ?
+        "dashed" :
+        "solid"
+      this.graphView.current.addEdge(state.selected.id, node.data('id'), style)
+      const targetName = node.data('label')
+      var text = `[#${state.linking}](note/${targetName}.md)`
+      this.insertTextAtCursor(text)
+    }
+    this.unsetFlags()
+  }
+
+  onSelectEdge = edge => {
+    this.unsetFlags()
+  }
+
+  onTapBackground = event => {
+    if (this.state.adding === "note") {
+      const graphView = this.graphView.current
+      const id = graphView.addNodeFromEvent(event)
+      const name = "Untitled"
+      const info = { ...empty_info, name: name }
+      graphView.cy.$(`[id='${id}']`).json({ info: info, label: name })
+    }
+    this.unsetFlags()
+  }
+
+  onRefresh = data => {
+    this.graphView.current.runLayout()
+  }
 
   onSelectNode = data => {
     this.setState({ ...this.state, selected: data })
@@ -55,13 +129,28 @@ class App extends Component {
       <Container fluid>
         <div className="graphview" id="graphview-container">
           <GraphView
+            onSelectEdge={this.onSelectEdge}
             onSelectNode={this.onSelectNode}
             onDeselectNode={this.onDeselectNode}
+            onTapBackground={this.onTapBackground}
+            onTapNode={this.onTapNode}
             ref={this.graphView}
-            />
+          />
         </div>
       </Container>,
       <Container fluid className="p-3">
+        <div className="mb-1">
+          <Button className="mb-1" variant="primary" onClick={this.onAddNote}>+ note</Button>{' '}
+          <Button className="mb-1" variant="secondary" onClick={this.onAddFrom}>
+            + from
+          </Button>{' '}
+          <Button className="mb-1" variant="dark" onClick={this.onAddCrossref}>
+            + crossref
+          </Button>{' '}
+          <Button className="mb-1" variant="danger" onClick={this.onRefresh}>
+            refresh
+          </Button>
+        </div>
         <Row>
           <Col xs="12" lg="8" xl="9">
             <MarkdownEditor
@@ -69,6 +158,7 @@ class App extends Component {
               onChange={this.updateMarkdown}
               visible={false}
               height={500}
+              ref={this.markdownEditor}
             />
           </Col>
           <Col xs="12" lg="4" xl="3">
--- src/GraphView.js
+++ src/GraphView.js
@@ -7,19 +7,14 @@ export default class GraphView extends Component {
     state = {
         w: 0,
         h: 0,
-        // elements: [
-        //     { data: { id: 'one', label: 'Node very large 1 2' }, position: { x: 100, y: 50 } },
-        //     { data: { id: 'two', label: 'Node 2' }, position: { x: 200, y: 50 } },
-        //     { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-        // ],
         loading: true
     }
 
+    runLayout() {
+        this.cy.layout(this.layout).run()
+    }
+
     componentDidMount() {
-        // this.setState({
-        //     w: window.innerWidth,
-        //     h: window.innerHeight
-        // })
         fetch('notes.json', { mode: 'no-cors' })
             .then(res => { return res.json() })
             .then(json => {
@@ -30,48 +25,44 @@ export default class GraphView extends Component {
                     h: 800
                 });
                 this.setUpListeners();
-                this.cy.layout(this.layout).run()
+                this.runLayout()
             })
     }
 
-    id_counter = 3
+    id_counter = 0
 
-    keys = []
-
-    key_down = (e) => {
-        this.keys[e.key] = true
+    addNodeFromEvent = event => {
+        const pos = event.position
+        const id = 'new_' + this.id_counter
+        const label = "Node " + this.id_counter++
+        const new_node = { data: { id: id, label: label }, position: pos }
+        this.cy.add(new_node).select()
+        return id
     }
 
-    key_up = (e) => {
-        this.keys[e.key] = false
+    addEdge = (source, target, style) => {
+        this.cy.add({
+            group: 'edges',
+            data: {
+                source: source,
+                target: target,
+                style: style
+            }
+        })
+        this.cy.$(`[id=${source}]`).select()
     }
 
     setUpListeners = () => {
         this.cy.on('tap', 'node', (event) => {
-            // Add new edge
-            if (this.keys["Control"]) {
-                const nds = this.cy.elements(":selected");
-                if (nds.length > 0 && nds[0]) {
-                    const source = nds[0].data('id')
-                    const target = event.target.data('id')
-                    this.cy.add({
-                        group: 'edges',
-                        data: {
-                            source: source,
-                            target: target
-                        }
-                    });
-                }
-            }
+            this.props.onTapNode(event.target)
         })
         this.cy.on('tap', (event) => {
             if (event.target === this.cy) {
-                const pos = event.position
-                const id = 'new_' + this.id_counter
-                const label = "Node " + this.id_counter++
-                const new_node = { data: { id: id, label: label }, position: pos }
-                this.cy.add(new_node)
+                this.props.onTapBackground(event)
             }
+        })
+        this.cy.on('select', 'edge', event => {
+            this.props.onSelectEdge(event.target)
         })
         this.cy.on('select', 'node', (event) => {
             this.props.onSelectNode(event.target.data())
@@ -79,8 +70,6 @@ export default class GraphView extends Component {
         this.cy.on('unselect', 'node', (event) => {
             this.props.onDeselectNode(event.target.data())
         })
-        window.addEventListener("keydown", this.key_down, false)
-        window.addEventListener('keyup', this.key_up, false)
     }
 
 
